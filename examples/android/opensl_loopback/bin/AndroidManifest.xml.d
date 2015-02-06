868ac49e55d2302bcc601f574633f451add822e8
ubuntu <ubuntu@ubuntu.(none)>
2015-02-06 12:58:50 +0800

    delete examples/

--- examples/android/opensl_loopback/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/ubuntu/WebRTC/src/webrtc/examples/android/opensl_loopback/bin/AndroidManifest.xml : \
-/home/ubuntu/WebRTC/src/webrtc/examples/android/opensl_loopback/AndroidManifest.xml \
--- examples/android/opensl_loopback/bin/OpenSlDemo-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu/WebRTC/src/webrtc/examples/android/opensl_loopback/bin/OpenSlDemo-debug-unaligned.apk : \
-/home/ubuntu/WebRTC/src/webrtc/examples/android/opensl_loopback/bin/OpenSlDemo.ap_ \
-/home/ubuntu/WebRTC/src/webrtc/examples/android/opensl_loopback/bin/classes.dex \
-/home/ubuntu/WebRTC/src/webrtc/examples/android/opensl_loopback/libs/audio_device_module_java.jar \
-/home/ubuntu/WebRTC/src/webrtc/examples/android/opensl_loopback/libs/armeabi-v7a/libopensl-demo-jni.so \
