7b771b25a607b8c9c5b80582192400afd388ed41
ubuntu <ubuntu@ubuntu.(none)>
2015-02-06 13:24:08 +0800

    update

--- examples/android/media_demo/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/ubuntu/WebRTC/src/webrtc/examples/android/media_demo/bin/AndroidManifest.xml : \
-/home/ubuntu/WebRTC/src/webrtc/examples/android/media_demo/AndroidManifest.xml \
--- examples/android/opensl_loopback/gen/R.java.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu/WebRTC/src/webrtc/examples/android/opensl_loopback/gen/org/webrtc/app/R.java \
- : /home/ubuntu/WebRTC/src/webrtc/examples/android/opensl_loopback/res/drawable/logo.png \
-/home/ubuntu/WebRTC/src/webrtc/examples/android/opensl_loopback/res/layout/open_sl_demo.xml \
-/home/ubuntu/WebRTC/src/webrtc/examples/android/opensl_loopback/res/values/strings.xml \
-/home/ubuntu/WebRTC/src/webrtc/examples/android/opensl_loopback/bin/AndroidManifest.xml \
